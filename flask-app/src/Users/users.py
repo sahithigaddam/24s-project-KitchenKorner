6f7647a858fd91fe959dc2e967c1467392441b09
Amelia Willmann <willmann.a@northeastern.edu>
2024-04-12 15:19:30 -0400

    hey

---
+++ flask-app/src/Users/users.py
@@ -0,0 +1,24 @@
+# Customers 
+
+from flask import Blueprint, request, jsonify, make_response
+import json
+from src import db
+
+
+users = Blueprint('customers', __name__)
+
+# Get all customers from the DB
+@users.route('/users', methods=['GET'])
+def get_users():
+    cursor = db.get_db().cursor()
+    cursor.execute('select company, last_name,\
+        first_name, job_title, business_phone from customers')
+    row_headers = [x[0] for x in cursor.description]
+    json_data = []
+    theData = cursor.fetchall()
+    for row in theData:
+        json_data.append(dict(zip(row_headers, row)))
+    the_response = make_response(jsonify(json_data))
+    the_response.status_code = 200
+    the_response.mimetype = 'application/json'
+    return the_response

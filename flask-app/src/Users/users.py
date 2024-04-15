d8d31dd64a9c7b1df3d133e57fa0474b04fefe7b
Amelia Willmann <willmann.a@northeastern.edu>
2024-04-15 19:26:16 -0400

    #try

--- flask-app/src/Users/users.py
+++ flask-app/src/Users/users.py
@@ -5,30 +5,23 @@ import json
 from src import db
 
 
-users = Blueprint('customers', __name__)
-
-# return a list of all users
-@users.route('/users', methods=['GET'])
-def get_users():
-    cursor = db.get_db().cursor()
-    cursor.execute('select company, last_name,\
-        first_name, job_title, business_phone from customers')
-    row_headers = [x[0] for x in cursor.description]
-    json_data = []
-    theData = cursor.fetchall()
-    for row in theData:
-        json_data.append(dict(zip(row_headers, row)))
-    the_response = make_response(jsonify(json_data))
-    the_response.status_code = 200
-    the_response.mimetype = 'application/json'
-    return the_response
+users = Blueprint('Users', __name__)
 
 # Add a new user who joins the platform
-@users.route('/users/add', methods=['POST'])
+@users.route('/users', methods=['POST'])
 def add_user():
     data = request.get_json()
     cursor = db.get_db().cursor()
-    query = "INSERT INTO customers (company, last_name, first_name, job_title, business_phone) VALUES (%s, %s, %s, %s, %s)"
-    cursor.execute(query, (data['company'], data['last_name'], data['first_name'], data['job_title'], data['business_phone']))
+    query = "INSERT INTO users (User_ID, Username, Email, Full_Name, Created_At) VALUES (%s, %s, %s, %s, %s)"
+    cursor.execute(query, (data['User_ID'], data['Username'], data['Email'], data['Full_Name'], data['Created_At']))
     db.get_db().commit()
     return make_response(jsonify({"message": "User added successfully"}), 201)
+
+# Delete a user from the paltform
+@users.route('/users', methods=['DELETE'])
+def delete_user(user_id):
+    cursor = db.get_db().cursor()
+    query = "DELETE FROM customers WHERE id = %s"
+    cursor.execute(query, (user_id,))
+    db.get_db().commit()
+    return make_response(jsonify({"message": "User deleted successfully"}), 200)

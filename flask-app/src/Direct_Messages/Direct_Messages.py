9f6f1c6c3daf26f09415113d88455fbdedbc2186
Amelia Willmann <willmann.a@northeastern.edu>
2024-04-18 15:41:59 -0400

    dm fix2

--- flask-app/src/Direct_Messages/Direct_Messages.py
+++ flask-app/src/Direct_Messages/Direct_Messages.py
@@ -1,6 +1,6 @@
 # Direct messages 
 
-from flask import Blueprint, request, jsonify, make_response
+from flask import Blueprint, request, jsonify, make_response, current_app
 import json
 from src import db
 
@@ -14,11 +14,28 @@ def get_direct_message(sender_id):
     message = cursor.fetchone()
     return make_response(jsonify({"message": message}))
 
+
 @direct_messages.route('/direct_messages', methods=['POST'])
 def send_direct_message():
-    data = request.form()
+ 
+    # collecting data from the request object 
+    the_data = request.json
+    current_app.logger.info(the_data)
+
+    #extracting the variable
+    message = the_data['Message_Text']
+    receiver = the_data['Receiver_ID']
+
+    # Constructing the query
+    query = 'insert into Direct_Messages (Message_Text, Receiver_ID) values ("'
+    query += message + '", "'
+    query += receiver + ')'
+
+    current_app.logger.info(query)
+
+    # executing and committing the insert statement 
     cursor = db.get_db().cursor()
-    query = "INSERT INTO Direct_Messages (Receiver_ID, Sender_ID, Message_Text, Time_Sent) VALUES (%s, %s, %s, %s)"
-    cursor.execute(query, (data['Receiver_ID'], data['Sender_ID'], data['Message_Text'], data['Time_Sent']))
+    cursor.execute(query)
     db.get_db().commit()
-    return make_response(jsonify({"message": "Direct message sent successfully"}))
+    
+    return 'Message Sent!'

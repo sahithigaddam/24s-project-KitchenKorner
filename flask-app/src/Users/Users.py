e1c23bf277449b4887c2b5df7800db4e8684b78e
nicoleli26 <nnicoleli22@gmail.com>
2024-04-19 06:26:12 -0400

    Routes

--- flask-app/src/Users/Users.py
+++ flask-app/src/Users/Users.py
@@ -71,28 +71,35 @@ def add_new_user():
     
     return 'Success!'
 
-# Get the user's own posts
-@users.route('/users/<user_id>', methods=['GET'])
-def get_user_posts(user_id):
+# # Get the user's own posts
+# @users.route('/users/<user_id>', methods=['GET'])
+# def get_user_posts(user_id):
 
+#     user_query = "SELECT User_ID FROM Users ORDER BY Created_At DESC LIMIT 1"
+#     current_app.logger.info(user_query)
 
-    query = 'SELECT Posts.Post_ID, Username, Recipe_Name, Recipe_Image, Meal_Type,\
-        Cuisine, Expected_Time, Expected_Difficulty FROM Users JOIN Feeds ON Feeds.User_ID = Users.User_ID\
-        JOIN Follows ON Feeds.User_ID = Follows.Follower_ID\
-        JOIN Posts ON Posts.User_ID = Follows.Followee_ID JOIN Recipes ON Recipes.Post_ID = Posts.Post_ID WHERE Feeds.User_ID = ' + str(user_id)
-    current_app.logger.info(query)
+#     cursor = db.get_db().cursor()
+#     cursor.execute(user_query)
+#     user_result = cursor.fetchone()  # Fetch one row since we're expecting only one result
+#     user_id = user_result[0]  # Extract the user ID from the result
 
-    cursor = db.get_db().cursor()
-    cursor.execute(query)
-    column_headers = [x[0] for x in cursor.description]
-    json_data = []
-    the_data = cursor.fetchall()
-    for row in the_data:
-        json_data.append(dict(zip(column_headers, row)))
-    return jsonify(json_data)
+#     query = 'SELECT Posts.Post_ID, Username, Recipe_Name, Recipe_Image, Meal_Type,\
+#         Cuisine, Expected_Time, Expected_Difficulty FROM Users JOIN Feeds ON Feeds.User_ID = Users.User_ID\
+#         JOIN Follows ON Feeds.User_ID = Follows.Follower_ID\
+#         JOIN Posts ON Posts.User_ID = Follows.Followee_ID JOIN Recipes ON Recipes.Post_ID = Posts.Post_ID WHERE Feeds.User_ID = ' + str(user_id)
+#     current_app.logger.info(query)
+
+#     cursor = db.get_db().cursor()
+#     cursor.execute(query)
+#     column_headers = [x[0] for x in cursor.description]
+#     json_data = []
+#     the_data = cursor.fetchall()
+#     for row in the_data:
+#         json_data.append(dict(zip(column_headers, row)))
+#     return jsonify(json_data)
 
 # Get user's info
-@users.route('/mine/<user_id>', methods=['GET'])
+@users.route('/users/<user_id>', methods=['GET'])
 def get_user(user_id):
     
     user_query = "SELECT User_ID FROM Users ORDER BY Created_At DESC LIMIT 1"

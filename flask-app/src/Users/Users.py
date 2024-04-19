1e727223ab416e453b6bb394a99883d3fb144a41
Amelia Willmann <willmann.a@northeastern.edu>
2024-04-19 03:10:29 -0400

    Users

--- flask-app/src/Users/Users.py
+++ flask-app/src/Users/Users.py
@@ -17,7 +17,7 @@ def get_receiver_id(id):
     user_result = cursor.fetchone()  # Fetch one row since we're expecting only one result
     id = user_result[0]  # Extract the user ID from the result
 
-    query = 'SELECT Posts.Post_ID, Username, Recipe_Name, Recipe_Image, Meal_Type,\
+    query = 'SELECT Username, Recipe_Name, Recipe_Image, Meal_Type,\
         Cuisine, Expected_Time, Expected_Difficulty FROM Users JOIN Feeds ON Feeds.User_ID = Users.User_ID\
         JOIN Follows ON Feeds.User_ID = Follows.Follower_ID\
         JOIN Posts ON Posts.User_ID = Follows.Followee_ID JOIN Recipes ON Recipes.Post_ID = Posts.Post_ID WHERE Feeds.User_ID = ' + str(id)

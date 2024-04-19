80530a6e8e200e6196135fe872d36b47b1b97e45
nicoleli26 <nnicoleli22@gmail.com>
2024-04-19 08:46:57 -0400

    Tags

--- flask-app/src/Tags/Tags.py
+++ flask-app/src/Tags/Tags.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, request, jsonify, make_response
+from flask import Blueprint, request, jsonify, make_response, current_app
 import json
 from src import db
 
@@ -8,7 +8,7 @@ tags = Blueprint('tags', __name__)
 @tags.route('/tags/<post_id>/<user_id>', methods=['GET'])
 def get_tags(post_id, user_id):
     cursor = db.get_db().cursor()
-    cursor.execute('SELECT * FROM Tags WHERE Post_ID = %s AND User_ID = %s', (Post_ID, User_ID))
+    cursor.execute('SELECT * FROM Tags WHERE Post_ID = %s AND User_ID = %s', (post_id, user_id))
     row_headers = [x[0] for x in cursor.description]
     json_data = []
     theData = cursor.fetchall()
@@ -18,3 +18,26 @@ def get_tags(post_id, user_id):
     the_response.status_code = 200
     the_response.mimetype = 'application/json'
     return the_response
+
+# Add a tag to a post
+@tags.route('/tags/<post_id>/<user_id>', methods=['POST'])
+def tag_user():
+    the_data = request.json
+    current_app.logger.info(the_data)
+
+    #extracting the variable
+    post_id = the_data['Post_ID']
+    user_id = the_data["User_id"]
+
+    # Constructing the query
+    query = 'insert into Tags (Post_ID, User_ID) values ("'
+    query += post_id + '")'
+    query += user_id + '")'
+    current_app.logger.info(query)
+
+    # executing and committing the insert statement 
+    cursor = db.get_db().cursor()
+    cursor.execute(query)
+    db.get_db().commit()
+    
+    return 'Success!'

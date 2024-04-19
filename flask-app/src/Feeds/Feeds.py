0d90ca30056d6d8b12671f7ca8e181fec56352c4
nicoleli26 <nnicoleli22@gmail.com>
2024-04-19 05:09:50 -0400

    Feeds route

--- flask-app/src/Feeds/Feeds.py
+++ flask-app/src/Feeds/Feeds.py
@@ -11,10 +11,19 @@ feeds = Blueprint('feeds', __name__)
 @feeds.route('/feeds/<followee_id>', methods=['GET'])
 def get_feed(followee_id):
 
-    query = 'SELECT Posts.Post_ID, Username, Recipe_Name, Recipe_Image, Meal_Type,\
-        Cuisine, Expected_Time, Expected_Difficulty FROM Users JOIN Feeds ON Feeds.User_ID = Users.User_ID\
+    user_query = "SELECT User_ID FROM Users ORDER BY Created_At DESC LIMIT 1"
+    current_app.logger.info(user_query)
+
+    cursor = db.get_db().cursor()
+    cursor.execute(user_query)
+    user_result = cursor.fetchone()  # Fetch one row since we're expecting only one result
+    followee_id = user_result[0]  # Extract the user ID from the result
+
+    query = 'SELECT Posts.Post_ID, Username, Recipe_Name, Recipe_Image, Meal_Type, Cuisine, Expected_Time, Expected_Difficulty\
+        FROM Users JOIN Feeds ON Feeds.User_ID = Users.User_ID\
         JOIN Follows ON Feeds.User_ID = Follows.Follower_ID\
-        JOIN Posts ON Posts.User_ID = Follows.Followee_ID JOIN Recipes ON Recipes.Post_ID = Posts.Post_ID WHERE Feeds.User_ID = ' + str(followee_id)
+        JOIN Posts ON Posts.User_ID = Follows.Followee_ID\
+        JOIN Recipes ON Recipes.Post_ID = Posts.Post_ID WHERE Feeds.User_ID = ' + str(followee_id)
     current_app.logger.info(query)
 
     cursor = db.get_db().cursor()

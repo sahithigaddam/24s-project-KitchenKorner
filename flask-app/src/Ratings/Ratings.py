da94d1b0ef47703d1fd22272aeea064335965a2c
sahithigaddam <sahithig2003@gmail.com>
2024-04-19 08:25:46 -0400

    ratings

--- flask-app/src/Ratings/Ratings.py
+++ flask-app/src/Ratings/Ratings.py
@@ -166,23 +166,6 @@ def update_actual_difficulty_rating(post_id, actual_difficulty):
 
 
 
-# Returns all ratings under a post
-@ratings.route('/ratings/<post_id>', methods=['GET'])
-def get_ratings(post_id):
-    cursor = db.get_db().cursor()
-    cursor.execute('SELECT * FROM Ratings WHERE Post_ID = %s', (Post_ID))
-    row_headers = [x[0] for x in cursor.description]
-    json_data = []
-    theData = cursor.fetchall()
-    for row in theData:
-        json_data.append(dict(zip(row_headers, row)))
-    the_response = make_response(jsonify(json_data))
-    the_response.status_code = 200
-    the_response.mimetype = 'application/json'
-    return the_response
-
-
-
 
 
 
